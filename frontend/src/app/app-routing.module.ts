91eb45d6ac2d3c1a751bf701af91cdaafe614f31
Joel0347 <127158086+Joel0347@users.noreply.github.com>
2024-11-13 22:42:27 -0500

    feat_api(fe):added some modules, components and routing configuration

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuComponent } from './modules/global/components/menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MenuComponent,
+    loadChildren: () => import('./routes.module').then(m => m.AppRouterModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ frontend/src/app/routes.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+import { RouterModule  } from "@angular/router";
+import { globalRoutes } from "./modules/global/global.routing";
+
+@NgModule({
+  imports: [RouterModule.forChild([
+    ...globalRoutes
+  ])],
+  exports: [RouterModule]
+})
+
+export class AppRouterModule { }
