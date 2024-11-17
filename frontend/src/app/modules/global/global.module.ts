123e4b09afdedb352d82b3dd31a32ab7d45c6bf1
ClaudiaHdezPerez <claudia.hdez.perez.2004@gmail.com>
2024-11-16 20:13:19 -0800

    fix_api(fe):sidebar responsive

--- frontend/src/app/modules/global/global.module.ts
+++ frontend/src/app/modules/global/global.module.ts
@@ -5,6 +5,7 @@ import { MenuComponent } from './components/menu/menu.component';
 import { RouterModule } from '@angular/router';
 
 //add the route of each module you need yo import here down.
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -13,12 +14,14 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
 
     //add all modules you need here down. Remember to add them to export array too.
+    MatIconModule,
   ],
   exports: [
     //add the imported modules here down too.
+    MatIconModule,
   ]
 })
 export class GlobalModule { }
