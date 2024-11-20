0d1b63735e2fd20b55f8e1cb6eea02c6dde6f7b8
ClaudiaHdezPerez <claudia.hdez.perez.2004@gmail.com>
2024-11-20 05:34:45 -0800

    feat_api(fe): Added shared module and created generic table

--- frontend/src/app/modules/workCenter/workCenter.module.ts
+++ frontend/src/app/modules/workCenter/workCenter.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GlobalModule } from '../global/global.module';
+import { SharedModule } from '../../shared/shared.module';
 
 //work center components
 import { TotalConsumptionComponent } from './components/total-consumption/total-consumption.component';
 import { AvgConsumptionComponent } from './components/avg-consumption/avg-consumption.component';
 import { PolicyComponent } from './components/policy/policy.component';
-
 
 @NgModule({
   declarations: [
@@ -16,7 +16,8 @@ import { PolicyComponent } from './components/policy/policy.component';
   ],
   imports: [
     CommonModule,
-    GlobalModule
+    GlobalModule,
+    SharedModule
   ]
 })
 export class WorkCenterModule { }
