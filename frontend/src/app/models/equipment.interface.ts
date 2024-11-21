dec570e5f024eec7161c1b3a26d3ffe887f340cc
Joel0347 <127158086+Joel0347@users.noreply.github.com>
2024-11-21 17:29:15 -0500

    refactor_api(fe):moved some modules importation

---
+++ frontend/src/app/models/equipment.interface.ts
@@ -0,0 +1,3 @@
+export interface Equipment {
+  //add properties here
+}
--- frontend/src/app/modules/office/office.module.ts
+++ frontend/src/app/modules/office/office.module.ts
@@ -1,22 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GlobalModule } from '../global/global.module';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
+
+
 import { EquipmentComponent } from './components/equipment/equipment.component';
-import { SharedModule } from "../../shared/shared.module";
-import { ReactiveFormsModule } from '@angular/forms';
+
+
 @NgModule({
   declarations: [
     EquipmentComponent
   ],
   imports: [
     CommonModule,
-    GlobalModule,
-    SharedModule,
-    MatFormFieldModule,
-    MatSelectModule,
-    ReactiveFormsModule
+    GlobalModule
   ]
 })
 export class OfficeModule { }
