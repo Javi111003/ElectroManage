1a437fdf8e0decb8796a38579ca3cf6844c1559c
Javi111003 <128992433+Javi111003@users.noreply.github.com>
2024-11-25 13:22:24 -0500

    feat_api(be): Add Core of List Equipment Endpoint sending mock data

---
+++ backend/ElectroManage.Application/DTO's/ListEquipmentResponse.cs
@@ -0,0 +1,10 @@
+﻿using ElectroManage.Domain.Enums.Equipment;
+
+namespace ElectroManage.Application.DTO_s;
+
+public record ListEquipmentResponse
+{
+    public long Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string UseFrequency { get; set; } = string.Empty;
+}
