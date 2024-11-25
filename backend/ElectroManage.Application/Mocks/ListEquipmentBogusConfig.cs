380e85332cd9d957fdee0ccb2dce8a965b7b2049
Javi111003 <128992433+Javi111003@users.noreply.github.com>
2024-11-25 13:22:24 -0500

    feat_api(be): Add Core of List Equipment Endpoint sending mock data

---
+++ backend/ElectroManage.Application/Mocks/ListEquipmentBogusConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoBogus;
+using ElectroManage.Application.DTO_s;
+using ElectroManage.Domain.Enums.Equipment;
+
+namespace ElectroManage.Application.Mocks;
+public class ListEquipmentBogusConfig : AutoFaker<ListEquipmentResponse>
+{
+    public ListEquipmentBogusConfig()
+    {
+        RuleFor(x => x.Id, x => x.Random.Long(1, 1000));
+        RuleFor(x => x.Name, x => x.Random.Words(1));
+        RuleFor(x => x.UseFrequency, x => x.PickRandom<UseFrequency>().ToString());
+    }
+}
