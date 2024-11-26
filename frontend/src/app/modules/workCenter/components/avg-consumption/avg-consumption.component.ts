06c2ba3c7e35ddb0d2ad553eb3661ba73d9ac12e
Kendry <132113206+CyberKen10@users.noreply.github.com>
2024-11-26 12:46:34 -0500

    feat_api(fe):Added the page of average consumption

--- frontend/src/app/modules/workCenter/components/avg-consumption/avg-consumption.component.ts
+++ frontend/src/app/modules/workCenter/components/avg-consumption/avg-consumption.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { DateFilterFn } from '@angular/material/datepicker';
+import { Component, OnInit } from '@angular/core';
+import { ConfigColumn } from '../../../../shared/components/table/table.component';
+import { WorkCenterService } from '../../../../services/workCenter/work-center.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { WorkCenter } from '../../../../models/workCenter.interface';
+import { Register } from '../../../../models/register.interface';
 
 @Component({
   selector: 'app-avg-consumption',
@@ -6,11 +12,39 @@ import { Component } from '@angular/core';
   styleUrl: './avg-consumption.component.css',
 })
 export class AvgConsumptionComponent {
+  constructor (
+    private httpService: WorkCenterService
+  ) {}
   options: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
   selectedOptions: string[] = [];
+  isTableActive: boolean = false;
+  dataSources: { [key: string]: MatTableDataSource<any> } = {};
+  displayedColumns: ConfigColumn[] = [
+    {
+      title: 'Mes',
+      field: 'month'
+    },
+    {
+      title: 'Consumo Promedio (Kw/h)',
+      field: 'average'
+    },
+    {
+      title: 'Costo ($)',
+      field: 'cost'
+    }
+  ];
+  footerTable: any[] = [];
+
+  onConsultar() {
+    this.isTableActive = !this.isTableActive;
+  }
+  onProyeccion(){
+    alert("No esta implementado perro")
+  }
 
   handleSelectionChange(selected: string[]) {
     this.selectedOptions = selected;
     console.log('Opciones seleccionadas en el componente padre:', this.selectedOptions);
+    this.isTableActive=false;
   }
 }
