3d6a37f4f8109a2675365e06094dee7f96c4449f
Miguel <leyvadelacruzjosemiguel20@gmail.com>
2024-11-25 21:49:35 -0500

    feat_api(be): Finished List Equipments by Company

--- backend/ElectroManage.Application/DTO's/ListEquipmentByCompanyResponse.cs
+++ backend/ElectroManage.Application/DTO's/ListEquipmentByCompanyResponse.cs
@@ -1,9 +1,9 @@
-﻿using ElectroManage.Domain.Enums.Equipment;
-
+﻿
 namespace ElectroManage.Application.DTO_s;
 
-public record ListEquipmentResponse
+public record ListEquipmentByCompanyResponse
 {
+    public long CompanyId { get; set; } = 1;
     public long Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string UseFrequency { get; set; } = string.Empty;
---
+++ backend/ElectroManage.WebAPI/Endpoint/v1/Equipment/ListEquipmentByCompanyEndpoint.cs
@@ -0,0 +1,25 @@
+﻿using ElectroManage.Application.DTO_s;
+using ElectroManage.Application.Mocks;
+using System.Net;
+
+namespace ElectroManage.WebAPI.Endpoint.v1.Equipment;
+
+public class ListEquipmentByCompanyEndpoint : Endpoint<EmptyRequest, List<ListEquipmentByCompanyResponse>>
+{
+    public override void Configure()
+    {
+        Options(x => x.WithTags(RouteGroup.Equipment));
+        Tags(RouteGroup.Equipment);
+        Version(1);
+        AllowAnonymous();
+        Get("/equipment/companyId");
+        Summary(f => f.Summary = "List random equipments by company");
+    }
+
+    public override async Task HandleAsync(EmptyRequest req, CancellationToken ct)
+    {
+        var faker = new ListEquipmentByCompanyBogusConfig();
+        var data = faker.Generate(40);
+        await SendAsync(data, cancellation: ct);
+    }
+}
