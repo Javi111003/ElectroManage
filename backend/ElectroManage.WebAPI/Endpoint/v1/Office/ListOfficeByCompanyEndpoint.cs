86b4ff6a36615f523f26fed78adad346c508d5fd
Javi111003 <128992433+Javi111003@users.noreply.github.com>
2024-11-26 00:12:01 -0500

    feat_api(be):List Equipment By Office Endpoint sending mock data

--- backend/ElectroManage.WebAPI/Endpoint/v1/Office/ListOfficeByCompanyEndpoint.cs
+++ backend/ElectroManage.WebAPI/Endpoint/v1/Office/ListOfficeByCompanyEndpoint.cs
@@ -2,21 +2,23 @@
 using ElectroManage.Application.Mocks;
 
 namespace ElectroManage.WebAPI.Endpoint.v1.Office;
-public class ListOfficeEndpoint : Endpoint<EmptyRequest,List<ListOfficeResponse>>
+public class ListOfficeByCompanyEndpoint : Endpoint<EmptyRequest,List<ListOfficeResponse>>
 {
     public override void Configure()
     {
         Options(x => x.WithTags(RouteGroup.Office));
         Tags(RouteGroup.Office);
         Version(1);
-        Get("/office");
+        Get("/company/{companyId}/office");
         AllowAnonymous();
-        Summary(f => f.Summary = "List all offices");
+        Summary(f => f.Summary = "List all offices associated to a company");
     }
     public override async Task HandleAsync(EmptyRequest req, CancellationToken ct)
     {
-        var faker = new ListOfficeBogusConfig();
+        var companyId = Route<long>("companyId");
+        var faker = new ListOfficeByCompanyBogusConfig();
         var data = faker.Generate(20);
+        data.ForEach(d => d.CompanyId = companyId);
         await SendAsync(data, cancellation: ct);
     }
 }
