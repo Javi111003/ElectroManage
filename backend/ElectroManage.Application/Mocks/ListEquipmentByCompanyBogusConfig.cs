57fd12d390c42d20df13aa059bc5a6a9233dbb46
Miguel <leyvadelacruzjosemiguel20@gmail.com>
2024-11-25 21:49:35 -0500

    feat_api(be): Finished List Equipments by Company

--- backend/ElectroManage.Application/Mocks/ListEquipmentByCompanyBogusConfig.cs
+++ backend/ElectroManage.Application/Mocks/ListEquipmentByCompanyBogusConfig.cs
@@ -1,16 +1,23 @@
 ﻿using AutoBogus;
 using ElectroManage.Application.DTO_s;
 using ElectroManage.Domain.Enums.Equipment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ElectroManage.Application.Mocks;
-public class ListEquipmentBogusConfig : AutoFaker<ListEquipmentResponse>
+
+public class ListEquipmentByCompanyBogusConfig : AutoFaker<ListEquipmentByCompanyResponse>
 {
-    public ListEquipmentBogusConfig()
+    public ListEquipmentByCompanyBogusConfig()
     {
+        RuleFor(x => x.CompanyId, x => x.IndexFaker = 1);
         RuleFor(x => x.Id, x => x.Random.Long(1, 1000));
         RuleFor(x => x.Name, x => x.Random.Words(1));
         RuleFor(x => x.UseFrequency, x => x.PickRandom<UseFrequency>().ToString());
-        RuleFor(x => x.Model, x=> x.Random.Words(1));
+        RuleFor(x => x.Model, x => x.Random.Words(1));
         RuleFor(x => x.Efficency, x => x.Random.Decimal(1, 100));
         RuleFor(x => x.Brand, x => x.Random.Words(1));
         RuleFor(x => x.EquipmentType, x => x.Random.Words(1));
