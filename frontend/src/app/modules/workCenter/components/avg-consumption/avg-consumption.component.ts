2456f0743d18543a6d7bb7723e15671d8c013916
Joel0347 <127158086+Joel0347@users.noreply.github.com>
2024-11-26 23:02:14 -0500

    feat_api(fe):added new connection with equipment, company and office endpoints

--- frontend/src/app/modules/workCenter/components/avg-consumption/avg-consumption.component.ts
+++ frontend/src/app/modules/workCenter/components/avg-consumption/avg-consumption.component.ts
@@ -1,21 +1,21 @@
-import { DateFilterFn } from '@angular/material/datepicker';
 import { Component, OnInit } from '@angular/core';
 import { ConfigColumn } from '../../../../shared/components/table/table.component';
 import { WorkCenterService } from '../../../../services/workCenter/work-center.service';
 import { MatTableDataSource } from '@angular/material/table';
 import { WorkCenter } from '../../../../models/workCenter.interface';
-import { Register } from '../../../../models/register.interface';
 
 @Component({
   selector: 'app-avg-consumption',
   templateUrl: './avg-consumption.component.html',
   styleUrl: './avg-consumption.component.css',
 })
-export class AvgConsumptionComponent {
+export class AvgConsumptionComponent implements OnInit{
   constructor (
-    private httpService: WorkCenterService
+    private httpCenter: WorkCenterService
   ) {}
-  options: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
+
+  CenterOptions: string[] = [];
+  CenterObjects: WorkCenter[] = [];
   selectedOptions: string[] = [];
   isTableActive: boolean = false;
   dataSources: { [key: string]: MatTableDataSource<any> } = {};
@@ -35,16 +35,42 @@ export class AvgConsumptionComponent {
   ];
   footerTable: any[] = [];
 
-  onConsultar() {
+  ngOnInit() {
+    this.getWorkCenterList();
+    this.isTableActive = false;
+  }
+
+  /** * Retrieves the list of work centers from the WorkCenterService.
+  * Updates the CenterOptions and CenterObjects arrays with the fetched work centers.
+  */
+  getWorkCenterList() {
+    this.httpCenter.getWorkCenterList().subscribe(workCenters => {
+      this.CenterObjects = workCenters;
+      this.CenterOptions = workCenters.map(item => item.name);
+    })
+  }
+
+  /** * Toggles the visibility of the table.
+  * Called when the "Consultar" button is clicked.
+  */
+  onConsultClick() {
     this.isTableActive = !this.isTableActive;
   }
-  onProyeccion(){
+
+  /** * Calculate the proyection of consumption for the next 3 years of the
+  * selected work centers.
+  * Called when the "Proyección" button is clicked.
+  */
+  onProyectionClick(){
     alert("No esta implementado perro")
   }
 
+  /** * Handles changes in the selected options for work centers.
+  * Updates the selectedOptions array and deactivates the table.
+  * @param selected An array of selected options.
+  */
   handleSelectionChange(selected: string[]) {
     this.selectedOptions = selected;
-    console.log('Opciones seleccionadas en el componente padre:', this.selectedOptions);
     this.isTableActive=false;
   }
 }
--- frontend/src/app/services/office/office.service.ts
+++ frontend/src/app/services/office/office.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { API_URL } from '../../config/api.config';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Office } from '../../models/office.interface';
+import { Equipment } from '../../models/equipment.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +12,15 @@ export class OfficeService {
 
   constructor(private http: HttpClient) { }
 
-  private officeListUrl = API_URL + '/v1/office';
+  private officeListUrl = `${API_URL}/v1/company`;
 
-  getOfficeList(): Observable<Office[]> {
-    return this.http.get<Office[]>(this.officeListUrl);
+  getOfficeList(centerID: number): Observable<Office[]> {
+    const url = `${this.officeListUrl}/${centerID}/office`;
+    return this.http.get<Office[]>(url);
+  }
+
+  getEquipmentList(centerID: number, officeID: number): Observable<Equipment[]> {
+    const url = `${this.officeListUrl}/${centerID}/office/${officeID}/equipments`;
+    return this.http.get<Equipment[]>(url);
   }
 }
