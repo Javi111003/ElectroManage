25dba92995304b22caa1e0db5e73fd9de9458e6f
Miguel <leyvadelacruzjosemiguel20@gmail.com>
2024-11-28 16:17:41 -0500

    feat_api(be): List warnings by company, order by year

---
+++ backend/ElectroManage.Application/DTO's/ListWarningsByCompanyResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectroManage.Application.DTO_s;
+
+public record ListWarningsByCompanyResponse
+{
+    public long CompanyID { get; set; }
+    public ICollection<WarningDTO> Warnings { get; set; } = [];
+}
---
+++ backend/ElectroManage.Application/DTO's/WarningDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectroManage.Application.DTO_s;
+public record WarningDTO
+{
+    public int Month { get; set; }
+    public int Year { get; set; }
+    public decimal EstablishedLimit { get; set; }
+    public decimal Consumption { get; set; }
+}
