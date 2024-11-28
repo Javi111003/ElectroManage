9056200650c18a4606ec586edaa8c7d4638b3ac2
Miguel <leyvadelacruzjosemiguel20@gmail.com>
2024-11-28 16:17:41 -0500

    feat_api(be): List warnings by company, order by year

---
+++ backend/ElectroManage.WebAPI/Endpoint/v1/Company/ListWarningsByCompanyEndpoint.cs
@@ -0,0 +1,29 @@
+﻿using ElectroManage.Application.DTO_s;
+using ElectroManage.Application.Mocks;
+
+namespace ElectroManage.WebAPI.Endpoint.v1.Company;
+
+public class ListWarningsByCompanyEndpoint : Endpoint<EmptyRequest, ListWarningsByCompanyResponse>
+{
+    public override void Configure()
+    {
+        Options(x => x.WithTags(RouteGroup.Company));
+        Tags(RouteGroup.Company);
+        Version(1);
+        Get("company/list_warnings");
+        AllowAnonymous();
+        Summary(f => f.Summary = "Listing randoms warnings by company");
+    }
+
+    public async override Task HandleAsync(EmptyRequest req, CancellationToken ct)
+    {
+        var faker = new ListWarningsByCompanyBogusConfig();
+        var data = faker.Generate(30);
+        var response = new ListWarningsByCompanyResponse
+        {
+            CompanyID = 23,
+            Warnings = data.OrderBy(x => x.Year).Reverse().ToList()
+        };
+        await SendAsync(response: response, cancellation: ct);
+    }
+}
