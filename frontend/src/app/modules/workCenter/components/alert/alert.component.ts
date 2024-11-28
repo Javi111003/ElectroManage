b8bc768775276f7b67e24be5391b91eed415acb2
ClaudiaHdezPerez <claudia.hdez.perez.2004@gmail.com>
2024-11-28 03:07:55 -0800

    feat_api(fe): Implemented alert component

--- frontend/src/app/modules/workCenter/components/alert/alert.component.ts
+++ frontend/src/app/modules/workCenter/components/alert/alert.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { ConfigColumn } from '../../../../shared/components/table/table.component';
 
 @Component({
   selector: 'app-alert',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrl: './alert.component.css'
 })
 export class AlertComponent {
+  dataSource: MatTableDataSource<any> = new MatTableDataSource();
+  displayedColumns: ConfigColumn[] = [
+    {
+      title:'Mes/Año',
+      field:'date'
+    },
+    {
+      title:'Consumo (kw)',
+      field:'consumption'
+    },
+    {
+      title:'Límite Mensual',
+      field:'monthlyLimit'
+    }
+  ];
+  isTableActive: boolean = false;
+  CenterOptions: string[] = [
+    'Centro 1', 'Centro 2', 'Centro 3'
+  ];
 
+  onClick() {
+    this.isTableActive = !this.isTableActive;
+  }
+
+  handleOptionSelected($event: string) {
+    throw new Error('Method not implemented.');
+  }
 }
