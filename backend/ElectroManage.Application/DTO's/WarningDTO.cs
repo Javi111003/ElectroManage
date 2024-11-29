5f2e6d8d8e4cf85a6a162cf6d6711041eca259e4
Javi111003 <128992433+Javi111003@users.noreply.github.com>
2024-11-28 23:19:37 -0500

    feat_api(be): Add company id to response and recieved from endpoint route

--- backend/ElectroManage.Application/DTO's/WarningDTO.cs
+++ backend/ElectroManage.Application/DTO's/WarningDTO.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ElectroManage.Application.DTO_s;
+﻿namespace ElectroManage.Application.DTO_s;
 public record WarningDTO
 {
     public int Month { get; set; }
--- backend/ElectroManage.WebAPI/Endpoint/v1/Company/ListWarningsByCompanyEndpoint.cs
+++ backend/ElectroManage.WebAPI/Endpoint/v1/Company/ListWarningsByCompanyEndpoint.cs
@@ -10,18 +10,19 @@ public class ListWarningsByCompanyEndpoint : Endpoint<EmptyRequest, ListWarnings
         Options(x => x.WithTags(RouteGroup.Company));
         Tags(RouteGroup.Company);
         Version(1);
-        Get("company/list_warnings");
+        Get("company/{companyId}/list_warnings");
         AllowAnonymous();
         Summary(f => f.Summary = "Listing randoms warnings by company");
     }
 
     public async override Task HandleAsync(EmptyRequest req, CancellationToken ct)
     {
+        var companyId = Route<long>("companyId");
         var faker = new ListWarningsByCompanyBogusConfig();
         var data = faker.Generate(30);
         var response = new ListWarningsByCompanyResponse
         {
-            CompanyID = 23,
+            CompanyID = companyId,
             Warnings = data.OrderBy(x => x.Year).Reverse().ToList()
         };
         await SendAsync(response: response, cancellation: ct);
