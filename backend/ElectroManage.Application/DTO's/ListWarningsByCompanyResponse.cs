ccca576f8cef38c012bdbff282d2cc7dad8b793f
Javi111003 <128992433+Javi111003@users.noreply.github.com>
2024-11-28 23:19:37 -0500

    feat_api(be): Add company id to response and recieved from endpoint route

--- backend/ElectroManage.Application/DTO's/ListWarningsByCompanyResponse.cs
+++ backend/ElectroManage.Application/DTO's/ListWarningsByCompanyResponse.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ElectroManage.Application.DTO_s;
-
+﻿namespace ElectroManage.Application.DTO_s;
 public record ListWarningsByCompanyResponse
 {
     public long CompanyID { get; set; }
--- backend/ElectroManage.Application/Mocks/ListWarningsByCompanyBogusConfig.cs
+++ backend/ElectroManage.Application/Mocks/ListWarningsByCompanyBogusConfig.cs
@@ -1,20 +1,14 @@
 ﻿using AutoBogus;
 using ElectroManage.Application.DTO_s;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ElectroManage.Application.Mocks;
-
 public class ListWarningsByCompanyBogusConfig : AutoFaker<WarningDTO>
 {
     public ListWarningsByCompanyBogusConfig()
     {
         RuleFor(x => x.Month, x => x.Random.Int(1, 12));
         RuleFor(x => x.Year, x => x.Random.Int(2000, 2024));
-        RuleFor(x => x.Consumption, x => x.Random.Decimal(500, 1000));
+        RuleFor(x => x.Consumption, x => x.Random.Decimal(600, 1000));
         RuleFor(x => x.EstablishedLimit, x => x.Random.Decimal(250, 600));
     }
 }
