e18f41a08757a238ae5ef42106bc6d67e1ff49e8
Joel0347 <127158086+Joel0347@users.noreply.github.com>
2024-12-03 15:18:03 -0500

    fix_api(fe):fixed incompatibility bugs with a package

--- frontend/src/app/modules/global/components/login/login.component.ts
+++ frontend/src/app/modules/global/components/login/login.component.ts
@@ -5,14 +5,22 @@ import { AuthService } from '../../../../services/auth/auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'] // Asegúrate de que esta ruta sea correcta
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
   username = 'user';
   password = 'password';
 
-  constructor(private authService: AuthService, private router: Router) {}
-  
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  /**
+   * Attempts to log in the user with the provided credentials.
+   * If the credentials are valid, navigates to the root route.
+   * Otherwise, displays an alert with an error message.
+   */
   login() {
     if (this.authService.login(this.username, this.password)) {
       this.router.navigate(['']);
--- frontend/src/app/modules/workCenter/components/excess/excess.component.ts
+++ frontend/src/app/modules/workCenter/components/excess/excess.component.ts
@@ -2,21 +2,13 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
 import { FormControl } from '@angular/forms';
 import { provideMomentDateAdapter } from '@angular/material-moment-adapter';
 import { MatDatepicker } from '@angular/material/datepicker';
-
-// Depending on whether rollup is used, moment needs to be imported differently.
-// Since Moment.js doesn't have a default export, we normally need to import using the `* as`
-// syntax. However, rollup creates a synthetic default module and we thus need to import it using
-// the `default as` syntax.
 import _moment from 'moment';
-// tslint:disable-next-line:no-duplicate-imports
 import {default as _rollupMoment, Moment} from 'moment';
 import { MatTableDataSource } from '@angular/material/table';
 import { ConfigColumn } from '../../../../shared/components/table/table.component';
 
 const moment = _rollupMoment || _moment;
 
-// See the Moment.js docs for the meaning of these formats:
-// https://momentjs.com/docs/#/displaying/format/
 export const MY_FORMATS = {
   parse: {
     dateInput: 'MM/YYYY',
