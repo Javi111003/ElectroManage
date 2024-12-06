19e747c5476b1a6e3d507917e586545394666ad6
Joel0347 <127158086+Joel0347@users.noreply.github.com>
2024-12-06 17:27:42 -0500

    refactor_api(fe): Adapted the structure for manager role

--- frontend/src/app/modules/workCenter/workCenter.module.ts
+++ frontend/src/app/modules/workCenter/workCenter.module.ts
@@ -3,19 +3,21 @@ import { CommonModule } from '@angular/common';
 import { GlobalModule } from '../global/global.module';
 
 //work center components
-import { TotalConsumptionComponent } from './components/total-consumption/total-consumption.component';
+import { RegisterComponent } from './components/register/index/register.component';
 import { AvgConsumptionComponent } from './components/avg-consumption/avg-consumption.component';
 import { PolicyComponent } from './components/policy/policy.component';
 import { AlertComponent } from './components/alert/alert.component';
 import { ExcessComponent } from './components/excess/excess.component';
+import { RegisterFormComponent } from './components/register/form/register-form.component';
 
 @NgModule({
   declarations: [
-    TotalConsumptionComponent,
+    RegisterComponent,
     AvgConsumptionComponent,
     PolicyComponent,
     AlertComponent,
-    ExcessComponent
+    ExcessComponent,
+    RegisterFormComponent
   ],
   imports: [
     CommonModule,
