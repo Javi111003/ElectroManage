0bc2d4af5dfd4d709fd8407c82c51cd9a97fce4a
Miguel <leyvadelacruzjosemiguel20@gmail.com>
2024-12-09 01:38:30 -0500

    feat_api(be): Implemented Put operation for Location Entity

---
+++ backend/ElectroManage.WebAPI/Endpoint/v1/Location/EditGeneralDataLocation.cs
@@ -0,0 +1,22 @@
+﻿using ElectroManage.Application.Features.Location.Command.Put;
+
+namespace ElectroManage.WebAPI.Endpoint.v1.Location;
+
+public class EditGeneralDataLocationEndpoint : Endpoint<EditGeneralDataLocationCommand, EditGeneralDataLocationResponse>
+{
+    public override void Configure()
+    {
+        Options(x => x.WithTags(RouteGroup.Location));
+        Tags(RouteGroup.Location);
+        Version(1);
+        Put("/location{id}");
+        AllowAnonymous();
+        Summary(f => f.Summary = "Edit general data of a Location");
+    }
+
+    public override async Task HandleAsync(EditGeneralDataLocationCommand req, CancellationToken ct)
+    {
+        var data = await req.ExecuteAsync(ct);
+        await SendAsync(data);
+    }
+}
