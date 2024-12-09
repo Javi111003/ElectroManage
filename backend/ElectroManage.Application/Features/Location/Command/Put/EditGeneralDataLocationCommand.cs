14654aa023889b0a63ef8e9ebab39127f686a4ce
Miguel <leyvadelacruzjosemiguel20@gmail.com>
2024-12-09 01:38:30 -0500

    feat_api(be): Implemented Put operation for Location Entity

---
+++ backend/ElectroManage.Application/Features/Location/Command/Put/EditGeneralDataLocationCommand.cs
@@ -0,0 +1,13 @@
+﻿using ElectroManage.Application.DTO_s;
+namespace ElectroManage.Application.Features.Location.Command.Put;
+
+public record EditGeneralDataLocationCommand : ICommand<EditGeneralDataLocationResponse> 
+{
+    public long Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public long ZipCode { get; set; }
+    public AddressDTO Address { get; set; } = null!;
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+}
