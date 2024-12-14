8bd89f608958dcfc0c17a635acc79d6a3395bf8c
ClaudiaHdezPerez <claudia.hdez.perez.2004@gmail.com>
2024-12-14 01:51:32 -0800

    feat_api(fe): added work center modal

--- frontend/src/app/modules/workCenter/components/manage/index/manage.component.ts
+++ frontend/src/app/modules/workCenter/components/manage/index/manage.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Office } from '../../../../../models/office.interface';
+import { WorkCenter } from '../../../../../models/workCenter.interface';
+import { ConfigColumn } from '../../../../../shared/components/table/table.component';
+
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-center-manage',
   templateUrl: './manage.component.html',
   styleUrl: './manage.component.css'
 })
-export class ManageComponent {
+export class ManageComponent implements OnInit {
+  dataSource: MatTableDataSource<any> = new MatTableDataSource();
+  displayedColumns: ConfigColumn[] = [
+    {
+      title: 'Nombre',
+      field: 'name'
+    },
+    {
+      title: 'Nombre Área Administrativa',
+      field: 'adminAreaName'
+    },
+    {
+      title: 'Descripción Área Administrativa',
+      field: 'adminAreaDescription'
+    },
+    {
+      title: 'Tipo de instalación',
+      field: 'instalationType'
+    },
+    {
+      title: 'Ubicación',
+      field: 'address'
+    },
+    {
+      title: 'Límite',
+      field: 'monthlyConsumptionLimit'
+    },
+    {
+      title: 'Fórmula de Costo Total',
+      field: 'formula'
+    },
+    {
+      title: 'Equipo Responsable',
+      field: 'teamWork'
+    }
+  ];
 
+  ngOnInit() {}
+
+  onClick(): void {
+      const modal = new bootstrap.Modal(document.getElementById('exampleModal') as HTMLElement);
+      modal.show();
+  }
 }
--- frontend/src/app/shared/components/button/button.component.ts
+++ frontend/src/app/shared/components/button/button.component.ts
@@ -8,6 +8,8 @@ import { Component, Input } from '@angular/core';
 export class ButtonComponent {
   @Input() label: string = 'Descargar PDF';
   @Input() function: (...args: any[])=>any = this.exportPDF;
+  @Input() showIcon: boolean = false;
+  @Input() icon: string = '';
 
   exportPDF() {}
 }
