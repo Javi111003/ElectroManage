29925861bac3e095cc87084aae261cb79094a035
Miguel <leyvadelacruzjosemiguel20@gmail.com>
2024-12-22 13:12:07 -0500

    fix_api(be): Implemented Office mapper

---
+++ backend/ElectroManage.Application/Mappers/OfficeMapper.cs
@@ -0,0 +1,21 @@
+﻿using ElectroManage.Application.Features.Office;
+
+namespace ElectroManage.Application.Mappers;
+
+public static class OfficeMapper
+{
+    public static OfficeResponse ToResponse(Domain.Entites.Offices.Office office)
+    {
+        return new OfficeResponse
+        {
+            Id = office.Id,
+            Name = office.Name,
+            Description = office.Description,
+            Company = new CompanyDTO
+            {
+                Id = office.Company.Id,
+                Name = office.Company.Name,
+            }
+        };
+    }
+}
