53d2ea8bad79f2d95b5d8d4ba5c7d1cb5ee1d6d8
Miguel <leyvadelacruzjosemiguel20@gmail.com>
2024-12-23 12:17:44 -0500

    feat_api(be): List mean cost during the last three years for companies

---
+++ backend/ElectroManage.Application/Features/Company/Query/MeanCostLastThreeYears/GetMeanCostLastThreeYearsCompanysQuery.cs
@@ -0,0 +1,6 @@
+﻿namespace ElectroManage.Application.Features.Company.Query.MeanCostLastThreeYears;
+
+public record GetMeanCostLastThreeYearsCompanysQuery : ICommand<GetMeanCostLastThreeYearsCompanysResponse>
+{
+    public ICollection<long> CompanyIds { get; set; } = [];
+}
