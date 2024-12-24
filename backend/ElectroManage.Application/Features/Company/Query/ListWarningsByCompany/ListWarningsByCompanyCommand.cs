f9c4b499446f77d5bef57585d33de149de4aae7a
Miguel <leyvadelacruzjosemiguel20@gmail.com>
2024-12-24 11:10:18 -0500

    feat_api(be): List Warning by company

---
+++ backend/ElectroManage.Application/Features/Company/Query/ListWarningsByCompany/ListWarningsByCompanyCommand.cs
@@ -0,0 +1,8 @@
+﻿using ElectroManage.Application.DTO_s;
+
+namespace ElectroManage.Application.Features.Company.Query.ListWarningsByCompany;
+
+public record ListWarningsByCompanyCommand : ICommand<ListWarningsByCompanyResponse>
+{
+    public long CompanyId { get; set; }
+}
---
+++ backend/ElectroManage.Application/Features/Company/Query/ListWarningsByCompany/ListWarningsByCompanyValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace ElectroManage.Application.Features.Company.Query.ListWarningsByCompany;
+
+public class ListWarningsByCompanyValidator : Validator<ListWarningsByCompanyCommand>
+{
+    public ListWarningsByCompanyValidator()
+    {
+        RuleFor(x => x.CompanyId)
+            .NotEmpty()
+            .WithMessage("The company Id cannot be empty")
+            .GreaterThan(0)
+            .WithMessage("The company Id must be positive");
+    }
+}
