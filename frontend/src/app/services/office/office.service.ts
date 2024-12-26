3062b88a285232f34807bca9bd814f3a83789530
Joel0347 <127158086+Joel0347@users.noreply.github.com>
2024-12-25 21:44:01 -0500

    feat_api(fe): added services for admin. areas, management teams, installation types, equipment types and brand

--- frontend/src/app/services/office/office.service.ts
+++ frontend/src/app/services/office/office.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { API_URL } from '../../config/api.config';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Office } from '../../models/office.interface';
-import { Equipment } from '../../models/equipment.interface';
+import { Equipment, EquipmentBrand, EquipmentType } from '../../models/equipment.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -37,5 +37,53 @@ export class OfficeService {
   getEquipmentList(centerID: number, officeID: number): Observable<Equipment[]> {
     const url = `${this.officeListUrl}/${centerID}/office/${officeID}/equipments`;
     return this.http.get<Equipment[]>(url);
+  }
+
+  /**
+   * Posts a new equipment brand to the API.
+   * This method sends an HTTP POST request to the API to create a new equipment brand.
+   * @param brand The EquipmentBrand object to be posted.
+   * @returns An Observable that resolves to the response from the API.
+   */
+  postEquipmentBrand(brand: EquipmentBrand): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+
+    return this.http.post<any>(`${API_URL}/v1/equipment_brand`, brand, { headers });
+  }
+
+  /**
+   * Deletes an equipment brand from the API.
+   * This method sends an HTTP DELETE request to the API to remove an equipment brand.
+   * @param id The ID of the equipment brand to be deleted.
+   * @returns An Observable that resolves to the response from the API.
+   */
+  deleteEquipmentBrand(id: number): Observable<any> {
+    return this.http.delete<any>(`${API_URL}/v1/equipment_brand/${id}`);
+  }
+
+  /**
+   * Posts a new equipment type to the API.
+   * This method sends an HTTP POST request to the API to create a new equipment type.
+   * @param type The EquipmentType object to be posted.
+   * @returns An Observable that resolves to the response from the API.
+   */
+  postEquipmentType(type: EquipmentType): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+
+    return this.http.post<any>(`${API_URL}/v1/equipment_type`, type, { headers });
+  }
+
+  /**
+   * Deletes an equipment type from the API.
+   * This method sends an HTTP DELETE request to the API to remove an equipment type.
+   * @param id The ID of the equipment type to be deleted.
+   * @returns An Observable that resolves to the response from the API.
+   */
+  deleteEquipmentType(id: number): Observable<any> {
+    return this.http.delete<any>(`${API_URL}/v1/equipment_type/${id}`);
   }
 }
