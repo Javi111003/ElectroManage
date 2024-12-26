6e1d80962660396cdd23f97d191291bbf065f26e
Joel0347 <127158086+Joel0347@users.noreply.github.com>
2024-12-25 21:04:46 -0500

    feat_api(fe): added equipment's type and brand models and center's admin. areas and installation models

--- frontend/src/app/models/workCenter.interface.ts
+++ frontend/src/app/models/workCenter.interface.ts
@@ -2,3 +2,18 @@ export interface WorkCenter {
   id: number;
   name: string;
 }
+
+export interface AdminArea {
+  name: string;
+  description: string | null;
+}
+
+export interface InstallationType {
+  name: string;
+  description: string | null;
+}
+
+export interface ManagementTeam {
+  name: string | null;
+  userIds: number[];
+}
