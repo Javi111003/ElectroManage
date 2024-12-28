954a8bd59414cfe28c19c57859cb9f043fc07d76
Javi111003 <128992433+Javi111003@users.noreply.github.com>
2024-12-27 22:26:39 -0500

    feat_api(be): Added authentication scheme for handle JWT, and implemented endpoints for register and login

---
+++ backend/ElectroManage.Application/Features/Auth/Register/RegisterUserCommandHandler.cs
@@ -0,0 +1,89 @@
+﻿using ElectroManage.Common.Dtos;
+using ElectroManage.Domain.DataAccess.Abstractions;
+using ElectroManage.Domain.Entites.Identity;
+using ElectroManage.WebAPI.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using User = ElectroManage.Domain.Entites.Identity.AppUser;
+
+namespace ElectroManage.Application.Features.Auth.Register;
+public class RegisterUserCommandHandler : CoreCommandHandler<RegisterModel, Response<NoContentData>>
+{
+    readonly ILogger<RegisterUserCommandHandler> _logger;
+    readonly RoleManager<AppRole> _roleManager;
+    readonly UserManager<User> _userManager;
+    readonly IConfiguration _configuration;
+    public RegisterUserCommandHandler(ILogger<RegisterUserCommandHandler> logger, RoleManager<AppRole> roleManager, UserManager<User> userManager, IConfiguration configuration, IUnitOfWork unitOfWork) : base(unitOfWork)
+    {
+        _logger = logger;
+        _roleManager = roleManager;
+        _userManager = userManager;
+        _configuration = configuration;
+    }
+
+    public override async Task<Response<NoContentData>> ExecuteAsync(RegisterModel command, CancellationToken ct = default)
+    {
+        _logger.LogInformation($"{nameof(ExecuteAsync)} | Execution started");
+        var companyRepository = UnitOfWork!.DbRepository<Domain.Entites.Sucursal.Company>();
+        var company = await companyRepository.FirstAsync(useInactive: true, filters: x => x.Id == command.CompanyId);
+        if(company == null)
+        {
+            _logger.LogError($"{nameof(ExecuteAsync)} | Company with id : {command.CompanyId} not found");
+            ThrowError($"Company with id : {command.CompanyId} not found", 404);
+        }
+        var appUser = new User
+        {
+            Email = command.Email,
+            UserName = command.Email,
+            Company = company
+        };
+        var result = await _userManager.CreateAsync(appUser, command.Password);
+        if (!result.Succeeded)
+        {
+            foreach (var error in result.Errors)
+            {
+                _logger.LogWarning($"{nameof(ExecuteAsync)} | Error creating new account -> {error.Description}");
+                AddError(error.Description);
+            }
+            ThrowIfAnyErrors();
+        }
+        var allowedRoles = _configuration["AllowedRoles"].Split(',').ToList();
+        var nonExistantRoles = command.Roles.Except(allowedRoles);
+        foreach(var role in nonExistantRoles)
+        {
+            _logger.LogError($"{nameof(ExecuteAsync)} | Role \"{role}\" not found");
+            AddError($"Role with id : \"{role}\" not found");
+        }
+        ThrowIfAnyErrors(404);
+        foreach (var rol in command.Roles)
+        {
+            if (!await _roleManager.RoleExistsAsync(rol))
+            {
+                var newRole = new AppRole { Name = rol };
+                var resultRole = await _roleManager.CreateAsync(newRole);
+                if (!resultRole.Succeeded)
+                {
+                    foreach (var error in resultRole.Errors)
+                    {
+                        _logger.LogWarning($"{nameof(ExecuteAsync)} | Error creating new role -> {error.Description}");
+                        AddError(error.Description);
+                    }
+                    ThrowIfAnyErrors();
+                }
+            }
+            var addToRoleResult = await _userManager.AddToRoleAsync(appUser, rol);
+            if (!addToRoleResult.Succeeded)
+            {
+                foreach (var error in addToRoleResult.Errors)
+                {
+                    _logger.LogWarning($"{nameof(ExecuteAsync)} | Error assigning role -> {error.Description}");
+                    AddError(error.Description);
+                }
+                ThrowIfAnyErrors();
+            }
+        }
+        _logger.LogInformation($"{nameof(ExecuteAsync)} | Execution completed");
+        return new Response<NoContentData>();
+    }
+}
--- backend/ElectroManage.WebAPI/ServiceExtensions.cs
+++ backend/ElectroManage.WebAPI/ServiceExtensions.cs
@@ -16,6 +16,8 @@ using System.Security.Claims;
 using System.Text;
 using ElectroManage.Application.Abstractions;
 using ElectroManage.Application.Services;
+using ElectroManage.Application.Abstractions.Authentication;
+using ElectroManage.Application.Services.Authentication;
 
 namespace ElectroManage.WebAPI;
 public static class ServiceExtensions
@@ -26,52 +28,67 @@ public static class ServiceExtensions
         services.AddScoped<IDateTimeService, DateTimeService>();
         services.AddScoped<IUnitOfWork, UnitOfWork>();
         services.AddScoped<ICheckUniqueService, CheckUniqueService>();
+        services.AddScoped<ITokenHandler, JwtHandler>();
         services.AddSingleton<IEmailSender<AppUser>, IdentityEmailSender>();
     }
     public static void AddGenericRepositoryExtension(this IServiceCollection services)
     {
         services.TryAddScoped(typeof(Domain.DataAccess.Abstractions.IGenericCoreRepository<>), typeof(GenericCoreRepository<>));
     }
-    public static void AddJWTAuthenticationExtension(this IServiceCollection services, IConfiguration configuration)
-    {
-        services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-            .AddJwtBearer(options =>
-            {
-                options.TokenValidationParameters = new TokenValidationParameters
+        public static void AddJWTAuthenticationExtension(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(options =>
                 {
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidateLifetime = true,
-                    ValidateIssuerSigningKey = true,
-                    ValidIssuer = configuration["TokenOptions:Issuer"],
-                    ValidAudience = configuration["TokenOptions:Audience"],
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["TokenOptions:SecurityKey"]))
-                };
-            });
-    }
-    public static void AddIdentityExtension(this IServiceCollection services) 
-    {   
-        services.AddIdentity<AppUser, AppRole>(options =>
-            {
-                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
-                options.Lockout.MaxFailedAccessAttempts = 5;
-                options.Lockout.AllowedForNewUsers = true;
-                options.Password.RequireDigit = true;
-                options.Password.RequireLowercase = true;
-                options.Password.RequireNonAlphanumeric = true;
-                options.Password.RequireUppercase = true;
-                options.Password.RequiredLength = 6;
-                options.Password.RequiredUniqueChars = 1;
-                options.ClaimsIdentity.UserIdClaimType = ClaimTypes.NameIdentifier;
-                options.SignIn.RequireConfirmedEmail = false;
-            })
-               .AddEntityFrameworkStores<ApplicationDbContext>()
-               .AddDefaultTokenProviders();
-    }
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = configuration["TokenOptions:Issuer"],
+                        ValidAudience = configuration["TokenOptions:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["TokenOptions:SecurityKey"]))
+                    };
+                    // Configuración para registrar eventos, como fallos de autenticación
+                    options.Events = new JwtBearerEvents
+                    {
+                        OnMessageReceived = context =>
+                        {
+                            Console.WriteLine($"Token recibido: {context.Request.Headers["Authorization"]}");
+                            return Task.CompletedTask;
+                        },
+                        OnAuthenticationFailed = context =>
+                        {
+                            Console.WriteLine($"Token inválido: {context.Exception.ToString()}");
+                            return Task.CompletedTask;
+                        }
+                    };
+                });
+        }
+        public static void AddIdentityExtension(this IServiceCollection services) 
+        {   
+            services.AddIdentity<AppUser, AppRole>(options =>
+                {
+                    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                    options.Lockout.MaxFailedAccessAttempts = 5;
+                    options.Lockout.AllowedForNewUsers = true;
+                    options.Password.RequireDigit = true;
+                    options.Password.RequireLowercase = true;
+                    options.Password.RequireNonAlphanumeric = true;
+                    options.Password.RequireUppercase = true;
+                    options.Password.RequiredLength = 6;
+                    options.Password.RequiredUniqueChars = 1;
+                    options.ClaimsIdentity.UserIdClaimType = ClaimTypes.NameIdentifier;
+                    options.SignIn.RequireConfirmedEmail = false;
+                })
+                   .AddEntityFrameworkStores<ApplicationDbContext>()
+                   .AddDefaultTokenProviders();
+        }
     public static void AddDbContextExtension(this IServiceCollection services, IConfiguration configuration)
     {
         if (configuration.GetValue<bool>("UseInMemoryDatabase"))
@@ -89,7 +106,11 @@ public static class ServiceExtensions
     }
     public static void AddAuthorizationPoliciesExtension(this IServiceCollection services, IConfiguration configuration)
     {
-        //Añadir policies para las constantes de autorización que se creen 
+        services.AddAuthorization(options =>
+        {
+            options.AddPolicy("AdminPolicy", policy => policy.RequireRole("Admin"));
+            options.AddPolicy("UserPolicy", policy => policy.RequireRole("User"));
+        });
     }
     public static IServiceProvider ApplyMigrationsExtension(this IServiceProvider serviceProvider)
     {
