df6dd4bfc20362bb8a8bb1dbecbe8a983ec20ce9
Javi111003 <128992433+Javi111003@users.noreply.github.com>
2024-12-28 13:00:58 -0500

    fix_api(be): Fixed response to include User general data

--- backend/ElectroManage.Application/Features/Auth/Login/LoginUserResponse.cs
+++ backend/ElectroManage.Application/Features/Auth/Login/LoginUserResponse.cs
@@ -1,7 +1,12 @@
-﻿using ElectroManage.Application.Identity.Models;
+﻿using ElectroManage.Application.DTO_s;
+using ElectroManage.Application.Identity.Models;
 
 namespace ElectroManage.Application.Features.Auth.Login;
 public record LoginUserResponse
 {
+    public long Id { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public CompanyDTO Company { get; set; } = null!;
+    public IEnumerable<string> Roles { get; set; } = new HashSet<string>();
     public AccessToken AccessToken { get; set; } = null!;
 }
