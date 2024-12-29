a7836ea01f1c22ac3272a27f56957aa97c3e6861
Miguel <leyvadelacruzjosemiguel20@gmail.com>
2024-12-29 01:03:08 -0500

    feat_api(be): Implemented get the five companies with most offices

---
+++ backend/ElectroManage.Application/Features/Company/Query/TopFiveCountOffice/GetTopFiveCompaniesCountOfficeCommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectroManage.Application.Features.Company.Query.TopFiveCountOffice;
+
+public record GetTopFiveCompaniesCountOfficeCommand : ICommand<GetTopFiveCompaniesCountOfficeResponse>
+{
+}
---
+++ backend/ElectroManage.Application/Features/Company/Query/TopFiveCountOffice/GetTopFiveCompaniesCountOfficeResponse.cs
@@ -0,0 +1,13 @@
+﻿using ElectroManage.Application.DTO_s;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectroManage.Application.Features.Company.Query.TopFiveCountOffice;
+
+public record GetTopFiveCompaniesCountOfficeResponse
+{
+    public IEnumerable<TopFiveCompanyCountOfficeDTO> TopFiveCompanyCounts { get; set; } = new HashSet<TopFiveCompanyCountOfficeDTO>();
+}
