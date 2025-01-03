b0eaa6ff4d940cc9f0d13be55807fe94d84b665a
Javi111003 <128992433+Javi111003@users.noreply.github.com>
2025-01-03 14:42:05 -0500

    fix_api(be): MIgration to do management team nullable at Company Module

--- backend/ElectroManage.Domain/Entites/Sucursal/Company.cs
+++ backend/ElectroManage.Domain/Entites/Sucursal/Company.cs
@@ -12,8 +12,8 @@ public class Company : Entity<long>
     public AministrativeArea AministrativeArea { get; set; } = null!;
     public long LocationId { get; set; }
     public Location Location { get; set; } = null!;
-    public long ManagementTeamId { get; set; }
-    public ManagementTeam ManagementTeam { get; set; } = null!;
+    public long? ManagementTeamId { get; set; }
+    public ManagementTeam? ManagementTeam { get; set; }
     public ICollection<AppUser> Workers { get; set; } = [];
     public ICollection<Office> Offices { get; set; } = [];
     public ICollection<Warning> Warnings { get; set; } = [];
