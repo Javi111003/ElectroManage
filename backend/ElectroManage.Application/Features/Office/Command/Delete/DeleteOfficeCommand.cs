96abdceb3329e6cb978273715b95651e1930231b
Miguel <leyvadelacruzjosemiguel20@gmail.com>
2025-01-02 20:45:17 -0500

    feat_api(be): Delete office endpoint

---
+++ backend/ElectroManage.Application/Features/Office/Command/Delete/DeleteOfficeCommand.cs
@@ -0,0 +1,8 @@
+﻿using ElectroManage.Common.Dtos;
+
+namespace ElectroManage.Application.Features.Office.Command.Delete;
+
+public record DeleteOfficeCommand : ICommand<Response<NoContentData>>
+{
+    public long Id { get; set; }
+}
