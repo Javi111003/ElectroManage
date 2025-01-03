5a5ac4695e49c46476d22c6da74747517e008ea6
Miguel <leyvadelacruzjosemiguel20@gmail.com>
2025-01-02 19:08:25 -0500

    fix_api(be): Create List Warning Mapper

---
+++ backend/ElectroManage.Application/Mappers/ListWarningMapper.cs
@@ -0,0 +1,21 @@
+﻿using ElectroManage.Application.DTO_s;
+namespace ElectroManage.Application.Mappers;
+
+public static class ListWarningMapper
+{
+    public static ListWarningsByCompanyResponse ToResponse(Domain.Entites.Sucursal.Company company)
+    {
+        return new ListWarningsByCompanyResponse
+        {
+            CompanyID = company.Id,
+            CompanyName = company.Name,
+            Warnings = company.Warnings.Select(w => new WarningDTO
+            {
+                Consumption = w.Consumption,
+                EstablishedLimit = w.EstablishedLimit,
+                Month = w.Created.Month,
+                Year = w.Created.Year,
+            }).ToList(),
+        };
+    }
+}
