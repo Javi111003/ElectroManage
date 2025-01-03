a33489707c0485a03ecf00d95762c5dcdb2296d8
Miguel <leyvadelacruzjosemiguel20@gmail.com>
2025-01-02 20:45:17 -0500

    feat_api(be): Delete office endpoint

---
+++ backend/ElectroManage.Application/Features/Office/Command/Delete/DeleteOfficeCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using ElectroManage.Common.Dtos;
+using ElectroManage.Domain.DataAccess.Abstractions;
+using Microsoft.Extensions.Logging;
+namespace ElectroManage.Application.Features.Office.Command.Delete;
+
+public class DeleteOfficeCommandHandler : CoreCommandHandler<DeleteOfficeCommand, Response<NoContentData>>
+{
+    readonly IUnitOfWork _unitOfWork;
+    readonly ILogger<DeleteOfficeCommandHandler> _logger;
+
+    public DeleteOfficeCommandHandler(IUnitOfWork unitOfWork, ILogger<DeleteOfficeCommandHandler> logger) : base(unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+        _logger = logger;
+    }
+
+    public override async Task<Response<NoContentData>> ExecuteAsync(DeleteOfficeCommand command, CancellationToken ct = default)
+    {
+        _logger.LogInformation($"{nameof(ExecuteAsync)} | Execution started");
+        var officeRepository = _unitOfWork.DbRepository<Domain.Entites.Offices.Office>();
+        var office = await officeRepository.FirstAsync(useInactive: true, filters: x => x.Id == command.Id);
+        if (office is null)
+        {
+            _logger.LogError($"Office with id {command.Id} not found");
+            ThrowError($"Office with id {command.Id} not found", 404);
+        }
+        office.StatusBaseEntity = Domain.Enums.StatusEntityType.Delete;
+        await officeRepository.UpdateAsync(office);
+        _logger.LogInformation($"{nameof(ExecuteAsync)} | Execution completed");
+        return Response<NoContentData>.SuccessWithOutData("OK");
+    }
+}
---
+++ backend/ElectroManage.WebAPI/Endpoint/v1/Office/DeleteOfficeEndpoint.cs
@@ -0,0 +1,23 @@
+﻿using ElectroManage.Application.Features.Office.Command.Delete;
+using ElectroManage.Common.Dtos;
+
+namespace ElectroManage.WebAPI.Endpoint.v1.Office;
+
+public class DeleteOfficeEndpoint : Endpoint<DeleteOfficeCommand, Response<NoContentData>>
+{
+    public override void Configure()
+    {
+        Options(x => x.WithTags(RouteGroup.Office));
+        Tags(RouteGroup.Office);
+        Version(1);
+        Delete("/office/{id}");
+        AllowAnonymous();
+        Summary(f => f.Summary = "Delete a Office");
+    }
+
+    public override async Task HandleAsync(DeleteOfficeCommand req, CancellationToken ct)
+    {
+        var data = await req.ExecuteAsync(ct);
+        await SendAsync(data);
+    }
+}
