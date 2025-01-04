9b94a364d2fff7edcf94a34912827905a9501558
Javi111003 <128992433+Javi111003@users.noreply.github.com>
2025-01-04 17:23:42 -0500

    fix_api(be): Removed deleted entities from lists entities associated to an office or a company

--- backend/ElectroManage.Application/Features/Office/Command/Delete/DeleteOfficeCommandHandler.cs
+++ backend/ElectroManage.Application/Features/Office/Command/Delete/DeleteOfficeCommandHandler.cs
@@ -18,11 +18,6 @@ public class DeleteOfficeCommandHandler : CoreCommandHandler<DeleteOfficeCommand
     public override async Task<Response<NoContentData>> ExecuteAsync(DeleteOfficeCommand command, CancellationToken ct = default)
     {
         _logger.LogInformation($"{nameof(ExecuteAsync)} | Execution started");
-        var companyRepository = _unitOfWork.DbRepository<Domain.Entites.Sucursal.Company>();
-        var include = new List<Expression<Func<Domain.Entites.Sucursal.Company, object>>>
-        {
-            x => x.Offices
-        };
         var officeRepository = _unitOfWork.DbRepository<Domain.Entites.Offices.Office>();
         var office = await officeRepository.FirstAsync(useInactive: true, filters: x => x.Id == command.Id);
         if (office is null)
@@ -30,10 +25,7 @@ public class DeleteOfficeCommandHandler : CoreCommandHandler<DeleteOfficeCommand
             _logger.LogError($"Office with id {command.Id} not found");
             ThrowError($"Office with id {command.Id} not found", 404);
         }
-        var company = await companyRepository.FirstAsync(filters: x => x.Id == office.CompanyId, includes: include);
-        company.Offices.Remove(office);
         office.StatusBaseEntity = Domain.Enums.StatusEntityType.Delete;
-        await companyRepository.UpdateAsync(company, false);   
         await officeRepository.UpdateAsync(office, false);
         await _unitOfWork.SaveChangesAsync();
         _logger.LogInformation($"{nameof(ExecuteAsync)} | Execution completed");
