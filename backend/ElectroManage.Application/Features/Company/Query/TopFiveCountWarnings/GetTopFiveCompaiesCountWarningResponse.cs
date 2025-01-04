a5f830dc538e4db672a1c486fb2720de6c8459fe
Miguel <leyvadelacruzjosemiguel20@gmail.com>
2025-01-03 23:11:46 -0500

    fix_api(be): Delete Response File

--- backend/ElectroManage.Application/Features/Company/Query/TopFiveCountWarnings/GetTopFiveCompaiesCountWarningResponse.cs
+++
@@ -1,7 +0,0 @@
-﻿using ElectroManage.Application.DTO_s;
-namespace ElectroManage.Application.Features.Company.Query.TopFiveCountWarnings;
-
-public record GetTopFiveCompaiesCountWarningResponse
-{
-    public IEnumerable<TopFiveCompaniesCountWarningDTO> TopFiveCompaniesCountWarnings { get; set; } = new HashSet<TopFiveCompaniesCountWarningDTO>();
-}
