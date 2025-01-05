61908b89bb62db2834528a3e40e6e26f85c8ed8f
Javi111003 <128992433+Javi111003@users.noreply.github.com>
2025-01-05 10:07:46 -0500

    fix_api(be): Replaced Collesctions at Dtos for Enumerables and some bugs solved

--- backend/ElectroManage.Application/Features/Company/Query/MeanCostLastThreeYears/GetMeanCostLastThreeYearsCompanysResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿using ElectroManage.Application.DTO_s;
-
-namespace ElectroManage.Application.Features.Company.Query.MeanCostLastThreeYears;
-
-public record GetMeanCostLastThreeYearsCompanysResponse
-{
-    public IEnumerable<ListMonthlastThreeYearsResponse> Response { get; set; } = new HashSet<ListMonthlastThreeYearsResponse>();
-}
--- backend/ElectroManage.WebAPI/Endpoint/v1/Company/ListMonthlastThreeYearsByCompanyEndpoint.cs
+++ backend/ElectroManage.WebAPI/Endpoint/v1/Company/ListMonthlastThreeYearsByCompanyEndpoint.cs
@@ -1,10 +1,8 @@
 ﻿using ElectroManage.Application.DTO_s;
 using ElectroManage.Application.Features.Company.Query.MeanCostLastThreeYears;
-using ElectroManage.Application.Mocks;
 
 namespace ElectroManage.WebAPI.Endpoint.v1.Company;
-
-public class ListMonthlastThreeYearsByCompanyEndpoint : Endpoint<GetMeanCostLastThreeYearsCompanysQuery, GetMeanCostLastThreeYearsCompanysResponse>
+public class ListMonthlastThreeYearsByCompanyEndpoint : Endpoint<GetMeanCostLastThreeYearsCompanysQuery, IEnumerable<ListMonthlastThreeYearsDto>>
 {
     public override void Configure()
     {
@@ -15,7 +13,6 @@ public class ListMonthlastThreeYearsByCompanyEndpoint : Endpoint<GetMeanCostLast
         AllowAnonymous();
         Summary(f => f.Summary = "List mean cost and mean consumption during the last three years for companies");
     }
-
     public override async Task HandleAsync(GetMeanCostLastThreeYearsCompanysQuery req, CancellationToken ct)
     {
         var data = await req.ExecuteAsync(ct);
