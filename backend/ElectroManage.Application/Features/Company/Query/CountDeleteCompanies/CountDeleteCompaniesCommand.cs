9a32ab08881a9c7ecb73dd4868c091f5a54fed8b
Javi111003 <128992433+Javi111003@users.noreply.github.com>
2025-01-05 12:03:35 -0500

    feat_api(be): Fixed Count deleted and created companies by year Query and adjusted useInactive at companies listings

--- backend/ElectroManage.Application/Features/Company/Query/CountDeleteCompanies/CountDeleteCompaniesCommand.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ElectroManage.Application.Features.Company.Query.CountDeleteCompanies;
-
-public record CountDeleteCompaniesCommand : ICommand<CountDeleteCompaniesResponse>
-{
-    public long Year { get; set; }
-}
---
+++ backend/ElectroManage.Application/Features/Company/Query/CountDeletedCompanies/CountDeletedCompaniesResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace ElectroManage.Application.Features.Company.Query.CountDeletedCompanies;
+public record CountDeletedCompaniesResponse
+{
+    public long CreatedComapniesThisYear { get; set; }
+    public long ExistingCompaniesThisYear { get; set; }
+    public long DeletedCompaniesThisYear { get; set; }
+}
