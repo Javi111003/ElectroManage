9dd126ba7406487ef2dace52fb878a9fb8394033
Javi111003 <128992433+Javi111003@users.noreply.github.com>
2025-01-05 09:37:04 -0500

    feat_api(be): Reimplemented logic of edit location general data for changes on location domain

--- backend/ElectroManage.Application/DTO's/LocationDTO.cs
+++ backend/ElectroManage.Application/DTO's/LocationDTO.cs
@@ -1,6 +1,7 @@
 ﻿namespace ElectroManage.Application.DTO_s;
 public record LocationDTO
 {
+    public long Id { get; set; }
     public string? AddressDetails { get; set; } = string.Empty;
     public CoordenateDTO CoordenateDTO { get; set; } = null!;
 }
