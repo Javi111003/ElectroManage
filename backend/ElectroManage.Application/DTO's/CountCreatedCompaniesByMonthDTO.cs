ac0b4f5836c890db5589a3a3c031266a406757b8
Miguel <leyvadelacruzjosemiguel20@gmail.com>
2025-01-06 23:25:14 -0500

    fix_api(be): Count Created and Deleted Companies by month, during a year

---
+++ backend/ElectroManage.Application/DTO's/CountCreatedCompaniesByMonthDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace ElectroManage.Application.DTO_s;
+
+public record CountCreatedDeletedCompaniesByMonthDTO
+{
+    public int Month { get; set; }
+    public int CountCreatedCompanies { get; set; }
+    public int CountDeletedCompanies { get; set; }
+}
--- backend/ElectroManage.Application/Features/Company/Query/CountDeletedCompanies/CountDeleteCompaniesQueryHandler.cs
+++ backend/ElectroManage.Application/Features/Company/Query/CountDeletedCompanies/CountDeleteCompaniesQueryHandler.cs
@@ -1,4 +1,5 @@
-﻿using ElectroManage.Domain.DataAccess.Abstractions;
+﻿using ElectroManage.Application.DTO_s;
+using ElectroManage.Domain.DataAccess.Abstractions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
@@ -19,11 +20,23 @@ public class CountDeleteCompaniesQueryHandler : CoreCommandHandler<CountDeletedC
         _logger.LogInformation($"{nameof(ExecuteAsync)} | Execution started");
         var companyRepository = _unitOfWork.DbRepository<Domain.Entites.Sucursal.Company>();
         var companies = await companyRepository.GetAll(useInactive: true).ToListAsync();
+        var companiesByMonth = new CountCreatedDeletedCompaniesByMonthDTO[12];
+        for (int month = 0; month < 12; month++)
+        {
+            companiesByMonth[month] = new CountCreatedDeletedCompaniesByMonthDTO
+            {
+                Month = month + 1,
+                CountCreatedCompanies = companies.Count(x => x.Created.Year == command.Year && x.Created.Month == month + 1),
+                CountDeletedCompanies = companies.Count(x => x.LastModified != null && x.LastModified.Value.Year == command.Year && x.StatusBaseEntity == Domain.Enums.StatusEntityType.Inactive && x.LastModified.Value.Month == month + 1)
+            };
+        }
         var countCompaniesResponse = new CountDeletedCompaniesResponse
         {
+            Year = command.Year,
             CreatedComapniesThisYear = companies.Count(x => x.Created.Year == command.Year),
             DeletedCompaniesThisYear = companies.Count(x => x.LastModified != null && x.LastModified.Value.Year == command.Year && x.StatusBaseEntity == Domain.Enums.StatusEntityType.Inactive),
             ExistingCompaniesThisYear = companies.Count(x => x.Created.Year <= command.Year && x.StatusBaseEntity != Domain.Enums.StatusEntityType.Inactive),
+            CompaniesByMonth = companiesByMonth
         };
         _logger.LogInformation($"{nameof(ExecuteAsync)} | Execution completed");
         return countCompaniesResponse;
