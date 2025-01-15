9dfb13139d79ee005a50d021825fd0016880f43d
Miguel <leyvadelacruzjosemiguel20@gmail.com>
2025-01-14 23:23:38 -0500

    fix_api(be): Count Warning per month of the Five Companies with most warnings

--- backend/ElectroManage.WebAPI/Endpoint/v1/DashBoard/GetTopFiveCompaniesCountWarning.cs
+++ backend/ElectroManage.WebAPI/Endpoint/v1/DashBoard/GetTopFiveCompaniesCountWarning.cs
@@ -3,22 +3,21 @@ using ElectroManage.Application.Features.Company.Query.TopFiveCountWarnings;
 
 namespace ElectroManage.WebAPI.Endpoint.v1.Dashboard;
 
-public class GetTopFiveCompaniesCountWarningEndpoint : Endpoint<EmptyRequest, IEnumerable<TopFiveCompaniesCountWarningDTO>>
+public class GetTopFiveCompaniesCountWarningEndpoint : Endpoint<GetTopFiveCompaiesCountWarningQuery, IEnumerable<TopFiveCompaniesCountWarningDTO>>
 {
     public override void Configure()
     {
         Options(x => x.WithTags(RouteGroup.Dashboard));
         Tags(RouteGroup.Dashboard);
         Version(1);
-        Get("/dashboard/top_five/company_count_warnings");
+        Get("/dashboard/top_five/company_count_warnings/{year}");
         AllowAnonymous();
         Summary(f => f.Summary = "Get top five companies with most warnings");
     }
 
-    public override async Task HandleAsync(EmptyRequest req, CancellationToken ct)
+    public override async Task HandleAsync(GetTopFiveCompaiesCountWarningQuery req, CancellationToken ct)
     {
-        var topFive = new GetTopFiveCompaiesCountWarningQuery();
-        var data = await topFive.ExecuteAsync(ct);
+        var data = await req.ExecuteAsync(ct);
         await SendAsync(data);
     }
 }
