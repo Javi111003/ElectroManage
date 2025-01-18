f2b2716657ff1ac09cf6194b7e3b876389a55f6f
Joel0347 <127158086+Joel0347@users.noreply.github.com>
2025-01-18 17:35:37 -0500

    fix_api(fe): validated dynamic fields for variables in cost formula

---
+++ frontend/src/app/directives/MathValidator/math-validator.directive.spec.ts
@@ -0,0 +1,8 @@
+import { MathValidatorDirective } from './math-validator.directive';
+
+describe('MathValidatorDirective', () => {
+  it('should create an instance', () => {
+    const directive = new MathValidatorDirective();
+    expect(directive).toBeTruthy();
+  });
+});
