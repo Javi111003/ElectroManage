705f61642fb85c4fcb0ddd1d7733749b0ebbe68a
Miguel <leyvadelacruzjosemiguel20@gmail.com>
2025-01-19 20:34:07 -0500

    feat_api(be): List registers by Company

---
+++ backend/ElectroManage.Application/DTO's/RegisterDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace ElectroManage.Application.DTO_s;
+
+public record RegisterDTO
+{
+    public long Id { get; set; }
+    public DateTime Date { get; set; }
+    public double Cost { get; set; }
+    public double Consumption { get; set; }
+}
---
+++ backend/ElectroManage.WebAPI/Endpoint/v1/Company/ListRegistersByCompanyEndpoint.cs
@@ -0,0 +1,23 @@
+﻿using ElectroManage.Application.DTO_s;
+using ElectroManage.Application.Features.Company.Query.ListRegistersByCompany;
+
+namespace ElectroManage.WebAPI.Endpoint.v1.Company;
+
+public class ListRegistersByCompanyEndpoint : Endpoint<ListRegistersByCompanyCommand, IEnumerable<RegisterDTO>>
+{
+    public override void Configure()
+    {
+        Options(x => x.WithTags(RouteGroup.Company));
+        Tags(RouteGroup.Company);
+        Version(1);
+        Get("/company/{id}/registers");
+        AllowAnonymous();
+        Summary(f => f.Summary = "List Registers by Company");
+    }
+
+    public override async Task HandleAsync(ListRegistersByCompanyCommand req, CancellationToken ct)
+    {
+        var data = await req.ExecuteAsync(ct);
+        await SendAsync(data);
+    }
+}
