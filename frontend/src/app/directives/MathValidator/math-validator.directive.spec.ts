605e4a63bec5e06d75ed39032fa956efcb41072c
Joel0347 <127158086+Joel0347@users.noreply.github.com>
2025-01-20 01:16:07 -0500

    fix_api(fe): fixed tests bugs on deploy

--- frontend/src/app/directives/MathValidator/math-validator.directive.spec.ts
+++ frontend/src/app/directives/MathValidator/math-validator.directive.spec.ts
@@ -2,7 +2,7 @@ import { MathValidatorDirective } from './math-validator.directive';
 
 describe('MathValidatorDirective', () => {
   it('should create an instance', () => {
-    const directive = new MathValidatorDirective();
+    const directive = new MathValidatorDirective([][0], [][0]);
     expect(directive).toBeTruthy();
   });
 });
