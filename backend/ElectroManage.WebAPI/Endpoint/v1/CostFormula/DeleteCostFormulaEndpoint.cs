432b6e77a9e3519e98e26de0d8137494395c613e
Javier Alejandro González Díaz <128992433+Javi111003@users.noreply.github.com>
2025-01-21 09:25:46 -0500

    fix_api(be): Fixed DeleteCostFormulaEndpoint.cs to request costFormula id On endpoint route

--- backend/ElectroManage.WebAPI/Endpoint/v1/CostFormula/DeleteCostFormulaEndpoint.cs
+++ backend/ElectroManage.WebAPI/Endpoint/v1/CostFormula/DeleteCostFormulaEndpoint.cs
@@ -3,20 +3,21 @@ using ElectroManage.Common.Dtos;
 
 namespace ElectroManage.WebAPI.Endpoint.v1.CostFormula;
 
-public class DeleteCostFormulaEndpoint : Endpoint<DeleteCostFormulaCommand, Response<NoContentData>>
+public class DeleteCostFormulaEndpoint : Endpoint<EmptyRequest, Response<NoContentData>>
 {
     public override void Configure()
     {
         Options(x => x.WithTags(RouteGroup.CostFormula));
         Tags(RouteGroup.CostFormula);
         Version(1);
-        Delete("/cost_formula");
+        Delete("/cost_formula/{id}");
         AllowAnonymous();
         Summary(f => f.Summary = "Deletes a Cost Formula.");
     }
-    public async override Task HandleAsync(DeleteCostFormulaCommand req, CancellationToken ct)
+    public async override Task HandleAsync(EmptyRequest req, CancellationToken ct)
     {
-        var data = await req.ExecuteAsync(ct);
+        var query = new DeleteCostFormulaCommand() { Id = Route<long>("id") };
+        var data = await query.ExecuteAsync(ct);
         await SendAsync(data);
     }
 }
