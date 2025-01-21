53e9a512c829e68b099d6bae3fe6cfc9318b2e6c
Javi111003 <128992433+Javi111003@users.noreply.github.com>
2025-01-20 23:09:14 -0500

    fix_api(be): Fixed company creation with a efficiency policy applyed

---
+++ backend/ElectroManage.Domain.DataAccess/Migrations/20250121040058_EffPCompIentity.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ElectroManage.Domain.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class EffPCompIentity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Created",
+                table: "EfficiencyPolicyCompany",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastModified",
+                table: "EfficiencyPolicyCompany",
+                type: "timestamp without time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StatusBaseEntity",
+                table: "EfficiencyPolicyCompany",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "EfficiencyPolicyCompany");
+
+            migrationBuilder.DropColumn(
+                name: "LastModified",
+                table: "EfficiencyPolicyCompany");
+
+            migrationBuilder.DropColumn(
+                name: "StatusBaseEntity",
+                table: "EfficiencyPolicyCompany");
+        }
+    }
+}
--- backend/ElectroManage.Domain/Entites/Sucursal/EfficiencyPolicyCompany.cs
+++ backend/ElectroManage.Domain/Entites/Sucursal/EfficiencyPolicyCompany.cs
@@ -1,5 +1,8 @@
-﻿namespace ElectroManage.Domain.Entites.Sucursal;
-public class EfficiencyPolicyCompany
+﻿using ElectroManage.Domain.Entites.Identity;
+using ElectroManage.Domain.Enums;
+
+namespace ElectroManage.Domain.Entites.Sucursal;
+public class EfficiencyPolicyCompany : IEntity
 {
     public long EfficiencyPolicyId { get; set; }
     public EfficiencyPolicy EfficiencyPolicy { get; set; } = null!;
@@ -7,4 +10,7 @@ public class EfficiencyPolicyCompany
     public Company Company { get; set; } = null!;
     public DateTime ApplyingDate { get; set; } = DateTime.UtcNow;
     public DateTime? To { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime? LastModified { get; set; }
+    public StatusEntityType StatusBaseEntity { get; set; } = StatusEntityType.Active;
 }
