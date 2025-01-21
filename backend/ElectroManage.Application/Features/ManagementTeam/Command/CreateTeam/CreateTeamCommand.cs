49393c75993f589d583b27c2fd6295d75cfc27eb
kevinator47 <kmvega05@gmail.com>
2025-01-21 00:30:47 -0500

    fix_api(be): fixing bugs on Create Team

--- backend/ElectroManage.Application/Features/ManagementTeam/Command/CreateTeam/CreateTeamCommand.cs
+++ backend/ElectroManage.Application/Features/ManagementTeam/Command/CreateTeam/CreateTeamCommand.cs
@@ -4,6 +4,6 @@ namespace ElectroManage.Application.Features.ManagementTeam.Command.CreateTeam;
 public record CreateTeamCommand : ICommand<ManagementTeamDto>
 {
     public string? Name { get; set; }
-    public required long CompanyId { get; set; }
+    public long CompanyId { get; set; }
     public IEnumerable<long> UserIds { get; set; } = new HashSet<long>(); 
 }
