e5e109b45009ecadd7fdae7de02822bb7e3b0096
Javi111003 <128992433+Javi111003@users.noreply.github.com>
2025-01-27 16:03:18 -0500

    feat_api(be): Initial commit to add UnitTestingProject

---
+++ backend/ElectroManage.Application.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ElectroManage.Application.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void PrintHelloWorld_ShouldReturnString()
+        {
+            Console.Write("Hello World");
+        }
+    }
+}
