605df720a88f21d81ab6c9b76d83b4c4dff728a9
Miguel <leyvadelacruzjosemiguel20@gmail.com>
2025-01-27 12:12:45 -0500

    feat_api(be): List Warnings By Company, by year, separated by months

---
+++ backend/ElectroManage.Application/Features/Company/Query/ListWarningsByCompanyByMonth/ListWarningsByCompanyByMonthCommand.cs
@@ -0,0 +1,9 @@
+﻿using ElectroManage.Application.DTO_s;
+
+namespace ElectroManage.Application.Features.Company.Query.ListWarningsByCompanyByMonth;
+
+public record ListWarningsByCompanyByMonthCommand : ICommand<IEnumerable<CountWarningByMonthDTO>>
+{
+    public long CompanyId { get; set; }
+    public long? Year { get; set; }
+}
---
+++ backend/ElectroManage.WebAPI/Endpoint/v1/Company/ListWarningsByCompanyByMonthEndpoint.cs
@@ -0,0 +1,23 @@
+﻿using ElectroManage.Application.DTO_s;
+using ElectroManage.Application.Features.Company.Query.ListWarningsByCompanyByMonth;
+
+namespace ElectroManage.WebAPI.Endpoint.v1.Company;
+
+public class ListWarningsByCompanyByMonthEndpoint : Endpoint<ListWarningsByCompanyByMonthCommand, IEnumerable<CountWarningByMonthDTO>>
+{
+    public override void Configure()
+    {
+        Options(x => x.WithTags(RouteGroup.Company));
+        Tags(RouteGroup.Company);
+        Version(1);
+        Get("company/{CompanyId}/Warnings_ByMonth");
+        AllowAnonymous();
+        Summary(f => f.Summary = "List warnings by company by month");
+    }
+
+    public override async Task HandleAsync(ListWarningsByCompanyByMonthCommand req, CancellationToken ct)
+    {
+        var data = await req.ExecuteAsync(ct);
+        await SendAsync(data);
+    }
+}
