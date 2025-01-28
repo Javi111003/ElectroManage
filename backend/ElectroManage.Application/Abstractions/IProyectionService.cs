b569ba24499b407babc30c77bf8e11c90772ad29
Miguel <leyvadelacruzjosemiguel20@gmail.com>
2025-01-28 00:47:28 -0500

    feat_api(be): Proyection Services and Proyection Endpoint

---
+++ backend/ElectroManage.Application/Abstractions/IProyectionService.cs
@@ -0,0 +1,8 @@
+﻿using ElectroManage.Application.DTO_s;
+
+namespace ElectroManage.Application.Abstractions;
+
+public interface IProyectionService
+{
+    public Task<IEnumerable<ProyectionDTO>> CalculateProyectionsAsync(Domain.Entites.Sucursal.Company company);
+}
