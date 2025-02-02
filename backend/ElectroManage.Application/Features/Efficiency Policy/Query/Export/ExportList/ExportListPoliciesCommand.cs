55cfda25049cc4b3477c0e6a2f483968d87beecc
kevinator47 <kmvega05@gmail.com>
2025-02-02 16:47:28 -0500

    fix_api(be): fixing export Policies Endpoint

--- backend/ElectroManage.Application/Features/Efficiency Policy/Query/Export/ExportList/ExportListPoliciesCommand.cs
+++ backend/ElectroManage.Application/Features/Efficiency Policy/Query/Export/ExportList/ExportListPoliciesCommand.cs
@@ -1,8 +1,7 @@
 using System;
 namespace ElectroManage.Application.Features.Efficiency_Policy.Query.Export.List;
-public class ExportPoliciesByCompanyCommand : ICommand<byte[]>
+public class ExportListPoliciesCommand : ICommand<byte[]>
 {
     public long UserId { get; set; }
-    public long CompanyId { get; set; }
     public string Format { get; set; } = "pdf";
 }
