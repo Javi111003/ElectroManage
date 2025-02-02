76364a278c8013bfd7e7306a1a1cc773b72e8dc3
Javi111003 <128992433+Javi111003@users.noreply.github.com>
2025-02-02 02:13:56 -0500

    feat_api(be):Added Exporter2Pdf and first query exporting warnings list by company

--- backend/ElectroManage.Infraestructure/Plugins/Exporters/PdfExporter.cs
+++ backend/ElectroManage.Infraestructure/Plugins/Exporters/PdfExporter.cs
@@ -1,4 +1,5 @@
 ﻿using ElectroManage.Infraestructure.Services;
+using SelectPdf;
 
 namespace ElectroManage.Infraestructure.Plugins.Exporters;
 public class PdfExporter : IExporter
@@ -6,6 +7,18 @@ public class PdfExporter : IExporter
     public string Format => "pdf";
     public byte[] Export(string data)
     {
-        throw new NotImplementedException();
+        // Crear una instancia del convertidor HTML a PDF
+        HtmlToPdf converter = new HtmlToPdf();
+
+        // Convertir el HTML a un documento PDF
+        PdfDocument doc = converter.ConvertHtmlString(data);
+
+        // Guardar el documento PDF en un array de bytes
+        byte[] pdf = doc.Save();
+
+        // Cerrar el documento para liberar recursos
+        doc.Close();
+
+        return pdf;
     }
 }
